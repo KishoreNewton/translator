b763740c77d92d14a2822c50f8b0b83a2a0b03f3
Kishore Newton <kishorenke@gmail.com>
2020-08-06 22:32:39 +0530

    fixed connecting issue

--- includes/config.php
+++ includes/config.php
@@ -4,6 +4,7 @@
     date_default_timezone_set('America/Los_Angeles');
     try{
         $con = new PDO("mysql:dbname=translater;host=localhost", "root", "");
+        $con->exec("set names utf8");
         $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     } catch(PDOException $e) {
         exit("Connection failed: " . $e->getMessage());
